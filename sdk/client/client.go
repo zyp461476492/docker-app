db63896c115af917eadbe4b8bc497cfe8c586035
张蕴鹏 <13150337230@163.com>
2019-07-01 17:39:00 +0800

    [add] docker 版本信息查询

--- sdk/client/client.go
+++ sdk/client/client.go
@@ -1,13 +1,15 @@
 package client
 
 import (
+	"context"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
-	"github.com/zyp461476492/sync-app/types"
+	localType "github.com/zyp461476492/sync-app/types"
 	"log"
 	"strconv"
 )
 
-func GetClient(asset types.DockerAsset) *client.Client {
+func GetClient(asset localType.DockerAsset) *client.Client {
 	// tcp://192.168.184.123:2376
 	host := "tcp://" + asset.Ip + ":" + strconv.Itoa(asset.Port)
 	cli, err := client.NewClient(host, asset.Version, nil, nil)
@@ -18,4 +20,17 @@ func GetClient(asset types.DockerAsset) *client.Client {
 	}
 
 	return cli
+}
+
+func GetClientInfo(cli *client.Client) types.Info {
+	ctx := context.Background()
+
+	info, err := cli.Info(ctx)
+
+	if err != nil {
+		log.Fatalf("get client info fail")
+		return types.Info{}
+	}
+
+	return info
 }
