66ec4eaa565b0e0c40c3e6982bd0286e18aa9787
张蕴鹏 <13150337230@163.com>
2019-07-02 17:13:52 +0800

    [更新] server main

--- web/server.go
+++ web/server.go
@@ -1,25 +1,24 @@
-package web
+package main
 
 import (
+	"github.com/zyp461476492/sync-app/web/router"
 	"log"
 	"net/http"
 	"time"
 )
 
-func testCon(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func test() {
-	http.Handle("/test", nil)
-
+func main() {
+	router.ConfigRouter()
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           "127.0.0.1:8080",
 		Handler:        nil,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	log.Fatal(s.ListenAndServe())
+	for {
+		log.Fatal(s.ListenAndServe())
+	}
+
 }
