443b6a407bd58f38285dfc5bccf254b843d75446
zhangyp <13150337230@163.com>
2019-09-02 17:46:08 +0800

    websocket demo

--- web/router/image_router.go
+++ web/router/image_router.go
@@ -1,12 +1,23 @@
 package router
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/websocket"
 	"github.com/zyp461476492/docker-app/sdk/image"
 	"log"
 	"net/http"
 	"strconv"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func list(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
@@ -85,5 +96,34 @@ func history(w http.ResponseWriter, r *http.Request) {
 }
 
 func imagePull(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	c, err := upgrader.Upgrade(w, r, nil)
+	defer c.Close()
+	if err != nil {
+		log.Printf("err %s", err.Error())
+	}
+	assetId, err := strconv.Atoi(r.Form.Get("assetId"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	imageId := r.Form.Get("imageId")
+	fmt.Printf("%d %s", assetId, imageId)
+	res, stream := image.PullImage(2, "docker.io/library/redis")
+	if !res.Res {
+		c.WriteMessage(1, []byte("pull error"))
+		return
+	}
+
+	reader := bufio.NewReader(stream)
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			log.Printf("Read Error: %s", err)
+			return
+		}
+		fmt.Println(str)
+		//c.WriteMessage(1, []byte(str))
+		c.WriteJSON(str)
+	}
 
 }
