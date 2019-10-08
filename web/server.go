b47d320858a94d5e5781490d6a575f9250fb6278
zhangyp <13150337230@163.com>
2019-10-08 14:29:39 +0800

    add command args support

--- web/server.go
+++ web/server.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"flag"
 	"github.com/zyp461476492/docker-app/web/router"
 	"log"
 	"net/http"
 	"time"
 )
 
+var port string
+
 func main() {
+	flag.StringVar(&port, "port", "8080", "port")
+	flag.Parse()
+	log.Printf("listen port [%s]", port)
+
 	router.ConfigRouter()
+	addr := "0.0.0.0:" + port
 	s := &http.Server{
-		Addr:           "0.0.0.0:8080",
+		Addr:           addr,
 		Handler:        nil,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	log.Println("服务已启动")
+	log.Println("server is up")
 	log.Panicln(s.ListenAndServe())
+
 }
